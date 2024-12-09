de92b7578eeb12e5173a31149ae0a4421a434b3a
thebluetropics <thebluetropics@gmail.com>
2024-12-09 13:09:35 +0700

    add dedicated item group

--- src/main/java/io/github/thebluetropics/permasnow/PermasnowMod.java
+++ src/main/java/io/github/thebluetropics/permasnow/PermasnowMod.java
@@ -2,15 +2,10 @@ package io.github.thebluetropics.permasnow;
 
 import io.github.thebluetropics.permasnow.block.ModBlocks;
 import io.github.thebluetropics.permasnow.item.ModItems;
+import io.github.thebluetropics.permasnow.item.group.ModItemGroups;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.ItemGroups;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Objects;
 
 public class PermasnowMod implements ModInitializer {
   public static final String ID = "permasnow";
@@ -20,12 +15,6 @@ public class PermasnowMod implements ModInitializer {
   public void onInitialize() {
     ModBlocks.initialize();
     ModItems.initialize();
-
-    ItemGroupEvents.MODIFY_ENTRIES_ALL.register((itemGroup, fabricItemGroupEntries) -> {
-      if (Objects.equals(itemGroup, ItemGroups.getDefaultTab())) {
-        fabricItemGroupEntries.add(ModItems.ETERNAL_ICE.getDefaultStack());
-        fabricItemGroupEntries.add(ModItems.ETERNAL_SNOW.getDefaultStack());
-      }
-    });
+    ModItemGroups.initialize();
   }
 }
