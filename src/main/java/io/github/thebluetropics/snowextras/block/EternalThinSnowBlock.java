5b54b2446ebc01dd99e6f627e36d4549b3223e92
thebluetropics <thebluetropics@gmail.com>
2024-12-18 19:16:08 +0700

    add pick stack for eternal thin snow block

--- src/main/java/io/github/thebluetropics/snowextras/block/EternalThinSnowBlock.java
+++ src/main/java/io/github/thebluetropics/snowextras/block/EternalThinSnowBlock.java
@@ -1,9 +1,12 @@
 package io.github.thebluetropics.snowextras.block;
 
+import io.github.thebluetropics.snowextras.item.ModItems;
 import net.minecraft.block.BlockState;
+import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
+import net.minecraft.world.WorldView;
 
 public class EternalThinSnowBlock extends ThinSnowBlock {
   public EternalThinSnowBlock(Settings settings) {
@@ -13,4 +16,9 @@ public class EternalThinSnowBlock extends ThinSnowBlock {
   /// Force this thin snow block to not melt.
   @Override
   protected void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) { /* ... */ }
+
+  @Override
+  public ItemStack getPickStack(WorldView world, BlockPos pos, BlockState state) {
+    return new ItemStack(ModItems.ETERNAL_SNOW);
+  }
 }
