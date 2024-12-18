378893d97bbb811fe235a6b78c8c19c6325d4607
thebluetropics <thebluetropics@gmail.com>
2024-12-18 09:29:57 +0700

    remove `LargeEntitySpawnHelper` mixin

--- src/main/java/io/github/thebluetropics/snowextras/mixin/LargeEntitySpawnHelperMixin.java
+++
@@ -1,9 +0,0 @@
-package io.github.thebluetropics.snowextras.mixin;
-
-import net.minecraft.entity.LargeEntitySpawnHelper;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(LargeEntitySpawnHelper.class)
-public class LargeEntitySpawnHelperMixin {
-  // TODO
-}
