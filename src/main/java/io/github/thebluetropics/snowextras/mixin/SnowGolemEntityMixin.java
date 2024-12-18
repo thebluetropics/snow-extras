c0d2f73966014741f74f344cfc19a3d8e67f3b1a
thebluetropics <thebluetropics@gmail.com>
2024-12-18 09:46:26 +0700

    remove `SnowGolemEntity` mixin

--- src/main/java/io/github/thebluetropics/snowextras/mixin/SnowGolemEntityMixin.java
+++
@@ -1,9 +0,0 @@
-package io.github.thebluetropics.snowextras.mixin;
-
-import net.minecraft.entity.passive.SnowGolemEntity;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(SnowGolemEntity.class)
-public class SnowGolemEntityMixin {
-  // TODO
-}
