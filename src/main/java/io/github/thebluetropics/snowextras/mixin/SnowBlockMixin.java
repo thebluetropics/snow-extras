3e4fa32cb88760f59f9944ac9956ea534a401dcf
thebluetropics <thebluetropics@gmail.com>
2024-12-18 19:15:31 +0700

    implement `SnowBlock` mixin

--- src/main/java/io/github/thebluetropics/snowextras/mixin/SnowBlockMixin.java
+++ src/main/java/io/github/thebluetropics/snowextras/mixin/SnowBlockMixin.java
@@ -1,9 +1,49 @@
 package io.github.thebluetropics.snowextras.mixin;
 
+import io.github.thebluetropics.snowextras.block.ModBlocks;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.SnowBlock;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.WorldView;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Objects;
 
 @Mixin(SnowBlock.class)
 public class SnowBlockMixin {
-  // TODO
+  /// Allows snow to be placed on top of full block eternal snow.
+  @Inject(
+    at = @At("HEAD"),
+    method = "canPlaceAt",
+    cancellable = true
+  )
+  private void canPlaceAt(BlockState state, WorldView world, BlockPos pos, CallbackInfoReturnable<Boolean> info) {
+    BlockState lowerState = world.getBlockState(pos.down());
+
+    if (lowerState.isOf(ModBlocks.ETERNAL_SNOW) && Objects.equals(lowerState.get(SnowBlock.LAYERS), 8)) {
+      info.setReturnValue(true);
+    }
+  }
+
+  @Inject(
+    at = @At("HEAD"),
+    method = "canReplace",
+    cancellable = true
+  )
+  private void canReplace(BlockState state, ItemPlacementContext context, CallbackInfoReturnable<Boolean> info) {
+    // TODO
+  }
+
+  @Inject(
+    at = @At("HEAD"),
+    method = "getPlacementState",
+    cancellable = true
+  )
+  private void getPlacementState(ItemPlacementContext context, CallbackInfoReturnable<BlockState> info) {
+    // TODO
+  }
 }
